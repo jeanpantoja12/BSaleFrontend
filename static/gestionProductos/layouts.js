3b686043b92f732109b001dc170412188f255914
jean <titoj@LAPTOP-VG0HSMPN>
2021-06-03 16:06:34 -0500

    Deploy sanitize

--- static/gestionProductos/layouts.js
+++ static/gestionProductos/layouts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
     // Cargar datos del navbar y el footer
-    $("#nav-container").load("/nav.html");
-    $("#footer-container").load("/footer.html");
+    $("#nav-container").load("nav");
+    $("#footer-container").load("footer");
 })
